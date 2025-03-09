5bee425af9407e56b08146e0981f99c9589e4505
gilson176 <gilson_sjc@hotmail.com>
2025-03-09 11:19:50 +0000

    Desafio Xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,49 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+#include <stdio.h>
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int i = 1;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("Torre\n\n");
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    while  (i < 6)   // Aqui estou utilizando a estrutura de repetição while para a Torre
+    {
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+        printf("Direita %d\n\n",i);
+        i++;
+    }
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+
+printf("Bispo\n\n");
+
+    for(int i = 0; i <=4; i++){     //  Aqui estou utilizando a estrutura de repetição for para o Bispo
+
+printf("Cima\n");
+printf("Esquerda\n\n");
+
+
+
+
+}
+
+
+int r = 1;
+
+printf("Rainha\n\n");
+
+do{
+   printf("Esquerda %d\n",r);
+   r++;
+                                        // Aqui estou utilizando a estrutura de repetição do-while para Rainha
+
+
+
+ }while
+       ( r < 9);
+
 
     return 0;
 }
